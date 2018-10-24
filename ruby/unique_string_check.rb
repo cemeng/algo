bc592022155d7b32e210d22132d5b93f2d351dee
Felix Tjandrawibawa <cemeng@gmail.com>
2018-10-25 06:46:14 +1100

    Add without data structure

--- ruby/unique_string_check.rb
+++ ruby/unique_string_check.rb
@@ -20,3 +20,24 @@ end
 
 puts all_unique_characters?("abcde")
 puts all_unique_characters?("abcee")
+
+# without any data structure
+def all_unique_characters_no_data?(input)
+  input_chars = input.chars
+  input_size = input.size
+  return false if input_size > 128
+
+  input_chars.each_with_index do |c, i| # iteration O(n)
+    j = i + 1
+    while(j < input_size) do
+      return false if c == input_chars[j]
+      j = j + 1
+    end
+  end
+  # runtime O(n^2), space O(1)
+
+  true
+end
+
+puts all_unique_characters_no_data?("abcde")
+puts all_unique_characters_no_data?("abcee")
