96b8320ef9174dacc6c24bf65f8f54733cbfa6f1
Felix Tjandrawibawa <felix.tjandrawibawa@fairfaxmedia.com.au>
2018-11-23 16:24:56 +1100

    Working implementation - does not deal with duplicates

--- ruby/quick_sort.rb
+++ ruby/quick_sort.rb
@@ -1,10 +1,7 @@
 def quick_sort(inputs)
-  # find a pivot
-  # use the pivot value to put a "wall"
-
   return inputs if inputs.size == 1
   if inputs.size == 2
-    if inputs[0] > inputs[1]
+    if inputs[0] < inputs[1]
       return [inputs[0], inputs[1]]
     else
       return [inputs[1], inputs[0]]
@@ -13,30 +10,32 @@ def quick_sort(inputs)
 
   # pick the last element as pivot
   pivot = inputs[inputs.size-1]
-  puts "pivot #{pivot}"
 
   left_idx = 0
   right_idx = inputs.size-1
 
-  while left_idx != right_idx - 1
-    puts "looping {pivot} : #{left_idx} #{right_idx}"
-    if inputs[left_idx] > pivot && inputs[right_idx] < pivot
-      # swap
+  while left_idx < right_idx
+    puts "looping pivot value#{pivot} : left: #{left_idx} - #{inputs[left_idx]}, right: #{right_idx}: #{inputs[right_idx]}"
+    # swap
+    if inputs[left_idx] > pivot && inputs[right_idx] <= pivot
       left_value = inputs[left_idx]
       inputs[left_idx] = inputs[right_idx]
       inputs[right_idx] = left_value
+      left_idx = left_idx + 1
+      right_idx = right_idx - 1
     end
     # already correct
     if inputs[left_idx] < pivot
       left_idx = left_idx + 1
     end
-    if inputs[right_idx] > pivot
+    if inputs[right_idx] >= pivot
       right_idx = right_idx - 1
     end
   end
+  puts "result #{inputs}"
 
   (quick_sort(inputs[0..left_idx]) + quick_sort(inputs[left_idx+1..inputs.size-1])).flatten
 end
 
-puts "#{quick_sort([4,3,1,2,5,6])}"
+puts "#{quick_sort([4,7,1,2,5,6])}"
 
